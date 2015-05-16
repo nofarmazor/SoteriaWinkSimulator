a49bb693f58e6874d5a4aa8806418327770823d3
nofarmazor <nofarmazor@gmail.com>
2015-05-16 23:58:53 +0300

    Add command handler interface

---
+++ CommandHandler.py
@@ -0,0 +1,16 @@
+__author__ = 'nofar'
+
+import wink
+
+wink_api = wink.init("../config.cfg")
+
+def login():
+
+def get_active_devices():
+    devices = wink_api.get_devices()
+
+def turn_on(device_cloud_id):
+
+def turn_off(device_cloud_id):
+
+
